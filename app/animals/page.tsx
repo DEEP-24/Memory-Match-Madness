4c947f1f143b1c2b17bbf1a02f4eca4cf490209a
Kunwar Gautam <gautam@thinkroman.com>
2023-03-31 23:30:09 -0500

    chore: prettier format

--- app/animals/page.tsx
+++ app/animals/page.tsx
@@ -1,10 +1,10 @@
-"use client";
-import "~/styles/animals.css";
-import Link from "next/link";
-import * as React from "react";
-import { ArrowLeftIcon, ArrowPathIcon } from "@heroicons/react/20/solid";
-import classNames from "classnames";
-import { motion, useCycle } from "framer-motion";
+'use client';
+import '~/styles/animals.css';
+import Link from 'next/link';
+import * as React from 'react';
+import { ArrowLeftIcon, ArrowPathIcon } from '@heroicons/react/20/solid';
+import classNames from 'classnames';
+import { motion, useCycle } from 'framer-motion';
 
 type AnimalCard = {
   id: number;
@@ -18,113 +18,113 @@ type AnimalCard = {
 const ANIMAL_CARDS: AnimalCard[] = [
   {
     id: 1,
-    type: "elephant",
-    imageSrc: "/assets/elephant.jpg",
+    type: 'elephant',
+    imageSrc: '/assets/elephant.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 2,
-    type: "elephant",
-    imageSrc: "/assets/elephant.jpg",
+    type: 'elephant',
+    imageSrc: '/assets/elephant.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 3,
-    type: "lion",
-    imageSrc: "/assets/lion.avif",
+    type: 'lion',
+    imageSrc: '/assets/lion.avif',
     answered: false,
     selected: false,
   },
   {
     id: 4,
-    type: "lion",
-    imageSrc: "/assets/lion.avif",
+    type: 'lion',
+    imageSrc: '/assets/lion.avif',
     answered: false,
     selected: false,
   },
   {
     id: 5,
-    type: "tiger",
-    imageSrc: "/assets/tiger.jpg",
+    type: 'tiger',
+    imageSrc: '/assets/tiger.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 6,
-    type: "tiger",
-    imageSrc: "/assets/tiger.jpg",
+    type: 'tiger',
+    imageSrc: '/assets/tiger.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 7,
-    type: "monkey",
-    imageSrc: "/assets/monkey.jpg",
+    type: 'monkey',
+    imageSrc: '/assets/monkey.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 8,
-    type: "monkey",
-    imageSrc: "/assets/monkey.jpg",
+    type: 'monkey',
+    imageSrc: '/assets/monkey.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 9,
-    type: "giraffe",
-    imageSrc: "/assets/giraffe.avif",
+    type: 'giraffe',
+    imageSrc: '/assets/giraffe.avif',
     answered: false,
     selected: false,
   },
   {
     id: 10,
-    type: "giraffe",
-    imageSrc: "/assets/giraffe.avif",
+    type: 'giraffe',
+    imageSrc: '/assets/giraffe.avif',
     answered: false,
     selected: false,
   },
   {
     id: 11,
-    type: "dog",
-    imageSrc: "/assets/dog.jpg",
+    type: 'dog',
+    imageSrc: '/assets/dog.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 12,
-    type: "dog",
-    imageSrc: "/assets/dog.jpg",
+    type: 'dog',
+    imageSrc: '/assets/dog.jpg',
     answered: false,
     selected: false,
   },
   {
     id: 13,
-    type: "cat",
-    imageSrc: "/assets/cat.png",
+    type: 'cat',
+    imageSrc: '/assets/cat.png',
     answered: false,
     selected: false,
   },
   {
     id: 14,
-    type: "cat",
-    imageSrc: "/assets/cat.png",
+    type: 'cat',
+    imageSrc: '/assets/cat.png',
     answered: false,
     selected: false,
   },
   {
     id: 15,
-    type: "deer",
-    imageSrc: "/assets/deer.webp",
+    type: 'deer',
+    imageSrc: '/assets/deer.webp',
     answered: false,
     selected: false,
   },
   {
     id: 16,
-    type: "deer",
-    imageSrc: "/assets/deer.webp",
+    type: 'deer',
+    imageSrc: '/assets/deer.webp',
     answered: false,
     selected: false,
   },
@@ -137,17 +137,11 @@ const randomize = (array: any[]) => {
 
 function Animals() {
   const [isInitallyMounted, setIsInitallyMounted] = React.useState(false);
-  const [cards, setCards] = React.useState<AnimalCard[]>(
-    randomize(ANIMAL_CARDS)
-  );
+  const [cards, setCards] = React.useState<AnimalCard[]>(randomize(ANIMAL_CARDS));
   const [turns, setTurns] = React.useState(0);
   const [matches, setMatches] = React.useState(0);
-  const [selectedCardOneId, setSelectedCardOneId] = React.useState<
-    number | null
-  >(null);
-  const [selectedCardTwoId, setSelectedCardTwoId] = React.useState<
-    number | null
-  >(null);
+  const [selectedCardOneId, setSelectedCardOneId] = React.useState<number | null>(null);
+  const [selectedCardTwoId, setSelectedCardTwoId] = React.useState<number | null>(null);
 
   const restart = () => {
     setTurns(0);
@@ -174,7 +168,7 @@ function Animals() {
   }, [setIsInitallyMounted]);
 
   React.useEffect(() => {
-    console.log("isInitiallyMounted - ", isInitallyMounted);
+    console.log('isInitiallyMounted - ', isInitallyMounted);
   });
 
   React.useEffect(() => {
@@ -196,7 +190,7 @@ function Animals() {
     }
 
     if (cardOne.type !== cardTwo.type) {
-      console.log("not a match");
+      console.log('not a match');
       setSelectedCardOneId(null);
       setSelectedCardTwoId(null);
       setTurns((prevCount) => prevCount + 1);
@@ -214,7 +208,7 @@ function Animals() {
         }
 
         return card;
-      })
+      }),
     );
 
     setTurns((prevCount) => prevCount + 1);
@@ -226,9 +220,9 @@ function Animals() {
   return (
     <div
       style={{
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
       <div className="Matches_Turns">
@@ -250,13 +244,12 @@ function Animals() {
         <div
           className="card-container"
           style={{
-            height: "100%",
-            padding: "10px",
+            height: '100%',
+            padding: '10px',
           }}
         >
           {cards.map((card, i) => {
-            const isMatched =
-              selectedCardOneId === card.id || selectedCardTwoId === card.id;
+            const isMatched = selectedCardOneId === card.id || selectedCardTwoId === card.id;
 
             const isSelected = card.selected ? true : isMatched;
 
@@ -303,15 +296,14 @@ type CardProps = {
   isInitallyMounted: boolean;
 };
 
-const defaultUrl = "/assets/default.jpg";
+const defaultUrl = '/assets/default.jpg';
 
 function CardComponent(props: CardProps) {
   const [imageurl, setImageurl] = React.useState(defaultUrl);
   const [isFlipped, setIsFlipped] = React.useState(false);
   const [flipDirection, setFlipDirection] = React.useState(1);
 
-  const { selectCard, isSelected, card, index, imageSrc, isInitallyMounted } =
-    props;
+  const { selectCard, isSelected, card, index, imageSrc, isInitallyMounted } = props;
 
   const imageAnimateProps = {
     rotateY: 0,
@@ -345,33 +337,29 @@ function CardComponent(props: CardProps) {
         selectCard(card.id);
         handleCardClick();
       }}
-      className={classNames(`card`, isSelected ? "" : "")}
+      className={classNames(`card`, isSelected ? '' : '')}
       key={card.id}
       //for stagerring effect
       initial={{ opacity: 0, translateY: 30 }}
       animate={{ opacity: 1, translateY: 0 }}
-      transition={{ duration: 0.3, delay: index * 0.2, type: "tween" }}
+      transition={{ duration: 0.3, delay: index * 0.2, type: 'tween' }}
     >
       <motion.div
         className="card_content"
         onClick={handleCardClick}
-        style={{ maxWidth: "100px" }}
+        style={{ maxWidth: '100px' }}
         // initial={{}}
         onAnimationStart={() => {
-          console.log("Animation Started");
+          console.log('Animation Started');
           console.log(isFlipped);
           console.log(flipDirection);
         }}
         animate={{
-          rotateY: isInitallyMounted
-            ? isFlipped
-              ? 180 * flipDirection
-              : 0
-            : 0,
+          rotateY: isInitallyMounted ? (isFlipped ? 180 * flipDirection : 0) : 0,
         }}
         transition={{ duration: 0.5 }}
         onAnimationComplete={() => {
-          console.log("Animation Completed");
+          console.log('Animation Completed');
           console.log(isFlipped);
           console.log(flipDirection);
 
@@ -383,8 +371,8 @@ function CardComponent(props: CardProps) {
           src={imageurl}
           alt=""
           style={{
-            objectFit: "cover",
-            width: "100%",
+            objectFit: 'cover',
+            width: '100%',
           }}
           //should add the opacity
         />
